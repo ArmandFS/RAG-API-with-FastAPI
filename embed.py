1838e60ca32b6a277f21e9ff96bd71a3469de640
ArmandFS <actuallyarmand@gmail.com>
2026-01-27 23:37:14 +1300

    added some comments to test out git

--- embed.py
+++ embed.py
@@ -11,3 +11,5 @@ collection.add(documents=[text], ids=["k8s"])
 print("Embedding stored in ChromaDB")
 
 #(venv) and python embed.py
+#just testing for git status and commits
+#how about we commit ts
