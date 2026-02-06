f8315c5f5d4ce236e35aa31f4bf88b8c1612545d
ArmandFS <actuallyarmand@gmail.com>
2026-02-06 13:55:02 +1300

    added semantic tests and mock llm mode in app.py

--- app.py
+++ app.py
@@ -27,19 +27,27 @@ collection = chroma.get_or_create_collection("docs")
 #ollama_client = ollama.Client(host="http://host.docker.internal:11434")
 ollama_client = ollama.Client(host=OLLAMA_HOST)
 
+
+
+#addick mock llm mode for testing.
 @app.post("/query")
 def query(q: str):
     results = collection.query(query_texts=[q], n_results=1)
     context = results["documents"] [0][0] if results["documents"] else ""
     logging.info(f"/query asked: {q}")
 
-    answer = ollama_client.generate(
-        model="tinyllama",
-        prompt=f"Context:\n{context}\n\nQuestion: {q}\n\nAnswer clearly and concisely:"
-    )
+    use_mock = os.getenv("USE_MOCK_LLM", "0") == "1"
 
-
-    return {"answer": answer["response"]}
+    #if else statement for mock llm mode
+    if use_mock:
+        return {"answer": context}
+    else:
+        #use real LLM (in production mode)
+        answer = ollama.generate(
+            model="tinyllama",
+            prompt=f"Context:\n{context}\n\nQuestion: {q}\n\nAnswer clearly and concisely:"
+        )
+        return {"answer": answer["response"]}
 
 @app.post("/add")
 def add_knowledge(text: str):
